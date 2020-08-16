174d99dadd58ca7cfac928eb6cc626a0b446cc27
abhi204655 <abhi9780253712@gmail.com>
2020-08-16 15:54:16 +0530

    Completed module

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-const pathToIndex = path.resolve(__dirname + "../client/index.html");
+const pathToIndex = path.resolve(__dirname, "../client/index.html");
 
 app.use("/*", (req, res) => {
   res.sendFile(pathToIndex);
